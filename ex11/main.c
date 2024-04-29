da26e720e9da0bfc8817b4e80bf92adf642fbdbc
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2024-04-29 19:36:14 -0300

    modifiquei ex11

--- ex11/main.c
+++ ex11/main.c
@@ -3,50 +3,59 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define T 8
+#define T 5
 
 int vetor[T];
 
-void readVector();
+void readVector(int);
+void show(int);
 void orderVector(int);
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
-    srand(time(NULL));
 
-    readVector();
-    orderVector(T);
+    readVector(0);
+    show(0);
 
     for (int i = 0; i < T; ++i) {
-        printf("%3d", vetor[i]);
+        orderVector(0);
     }
-    printf("\n");
+
+    show(0);
 
     system("pause");
     return 0;
 }
 
-void readVector() {
-    for (int i = 0; i < T; ++i) {
-        vetor[i] = rand() % 50;
-        printf("%3d", vetor[i]);
+void readVector(int position) {
+    if (position < T) {
+        printf("Posição [%d]: ", position);
+        scanf("%d", &vetor[position]);
+        fflush(stdin);
+        system("cls");
+
+        readVector(position + 1);
     }
-    printf("\n");
 }
 
-void orderVector(int size) {
-    if (size == T) {
-        --size;
+void show(int position) {
+    if (position < T) {
+        printf("%3d", vetor[position]);
+        show(position + 1);
+    } else {
+        printf("\n");
     }
-    if (size > 0) {
-        for (int i = 0; i < size; ++i) {
-            if (vetor[size] < vetor[i]) {
-                int temp = vetor[size];
-                vetor[size] = vetor[i];
-                vetor[i] = temp;
-            }
+}
+
+void orderVector(int position) {
+    if (position < T - 1) {
+        if (vetor[position] > vetor[position + 1]) {
+            int temp = vetor[position];
+            vetor[position] = vetor[position + 1];
+            vetor[position + 1] = temp;
+            orderVector(position);
+        } else {
+            orderVector(position + 1);
         }
-        orderVector(size - 1);
     }
 }
