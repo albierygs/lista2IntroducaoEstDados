311b07af52b2fb516fb4dcdff6f9a453093b89f6
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2024-04-29 19:17:38 -0300

    modifiquei ex10

--- ex10/main.c
+++ ex10/main.c
@@ -1,40 +1,35 @@
 /*Escreva um programa com uma função recursivo para calcular e imprimir os 20 primeiros números primos.*/
 
 #include <locale.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void primos(int, int);
+void primos(int, int, int, int);
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
 
     printf("20 primeiros números primos: ");
-    primos(0, 1);
+    primos(1, 1, 0, 0);
 
     system("pause");
     return 0;
 }
 
-void primos(int number, int total) {
-    bool find = false;
-
-    if (total > 20) {
-        return;
-    }
-
-    for (int i = number + 1; find != true; ++i) {
-        int divisionsNumber = 0;
-        for (int j = 1; j <= i; ++j) {
-            if (i % j == 0) {
-                divisionsNumber++;
+void primos(int number, int divisor, int divisionNumber, int total) {
+    if (total < 20) {
+        if (divisor > number) {
+            if (divisionNumber == 2) {
+                printf("%3d", number);
+                primos(number + 1, 1, 0, ++total);
+            } else {
+                primos(number + 1, 1, 0, total);
             }
-        }
-        if (divisionsNumber == 2) {
-            printf("%d ", i);
-            primos(i, ++total);
-            find = true;
+        } else {
+            if (number % divisor == 0) {
+                divisionNumber++;
+            }
+            primos(number, divisor + 1, divisionNumber, total);
         }
     }
 }
