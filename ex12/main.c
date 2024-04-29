86e170276ccb4f420d1bd9c678a5727a8126330e
Albiery Gonçalves dos Santos <albierygoncalves2@gmail.com>
2024-04-29 18:58:15 -0300

    modifiquei ex12

--- ex12/main.c
+++ ex12/main.c
@@ -4,50 +4,67 @@ Exemplo: A soma dos elementos da diagonal secundária da matriz é 4 + 1 + 0 + 3 =
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#define T 5
+#define T 4
 
 int matriz[T][T];
 
-void readMatriz();
-
-int diagonalSum(int);
+void readMatriz(int, int);
+void show(int, int);
+void diagonalSum(int, int, int);
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
-    srand(time(NULL));
 
-    readMatriz();
-
-    printf("A soma da diagonal secundária é %d", diagonalSum(T));
+    readMatriz(0, 0);
+    show(0, 0);
+    diagonalSum(0, 0, 0);
 
     system("pause");
     return 0;
 }
 
-void readMatriz() {
-    for (int i = 0; i < T; ++i) {
-        for (int j = 0; j < T; ++j) {
-            matriz[i][j] = rand() % 50;
-            printf("%4d", matriz[i][j]);
+void readMatriz(int row, int column) {
+    if (row < T && column < T) {
+        printf("Posição [%d][%d]: ", (row + 1), (column + 1));
+        scanf("%d", &matriz[row][column]);
+        fflush(stdin);
+        system("cls");
+
+        if (column == T - 1) {
+            readMatriz(row + 1, 0);
+        } else {
+            readMatriz(row, column + 1);
         }
-        printf("\n");
     }
 }
 
-int diagonalSum(int row) {
-    if (row == T) {
-        --row;
-    }
+void show(int row, int column) {
+    if (row < T && column < T) {
+        printf("%3d", matriz[row][column]);
 
-    int result = 0;
-
-    if (row >= 0) {
-        for (int i = 0; i < T; ++i) {
-            if (row + i == T - 1) {
-                result = matriz[row][i] + diagonalSum(row - 1);
-            }
+        if (column == T - 1) {
+            printf("\n");
+            show(row + 1, 0);
+        } else {
+            show(row, column + 1);
         }
+    } else {
+        printf("\n\n");
     }
-    return result;
+}
+
+void diagonalSum(int row, int column, int sum) {
+    if (row < T && column < T) {
+        if (row + column == T -1) {
+            sum += matriz[row][column];
+        }
+
+        if (column == T -1) {
+            diagonalSum(row + 1, 0, sum);
+        } else {
+            diagonalSum(row, column + 1, sum);
+        }
+    } else {
+        printf("A soma da diagonal secundária é %d\n", sum);
+    }
 }
